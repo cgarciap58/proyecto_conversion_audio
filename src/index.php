909c7a7d594ef2c3ac057a02f50b60ccb7b6e3e2
cgarciap58 <cgarciap58@iesalbarregas.es>
2026-03-08 16:05:27 +0100

    primer commit

---
+++ src/index.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "Hola Mundo";
+
+// Estructura para el trabajo
+
+// 1. index.php - Página HOME con nuestros nombres y una bienvenida
+// 2. presentación.php - Página con explicaciones sobre formatos de audio
+// 3. transformador.php - Página con transformador de audio
+// 4. test.php - Página con formulario a modo de test, incluyendo preguntas sobre lo presentado y el uso del transformador
+
+?>
